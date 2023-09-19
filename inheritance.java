49ba32314bf4791408f293e5980409675d6bddb8
Shashank Suman <sumanshashank616@gmail.com>
2023-09-19 22:35:49 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,16 @@
+// //suber class
+// class Employee {
+// 	int salary = 60000;
+// }
+// //Sub class
+// class Engineer extends Employee {
+// 	int benefits = 10000;
+// }
+
+// class inheritance {
+// 	public static void main(String args[])
+// 	{
+// 		Engineer E1 = new Engineer();
+// 		System.out.println("Salary : " + E1.salary+ "\nBenefits : " + E1.benefits);
+// 	}
+// }
