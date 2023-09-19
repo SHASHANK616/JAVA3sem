54d05746a76159da7b88c18620aaf950d45706fc
Shashank Suman <sumanshashank616@gmail.com>
2023-09-19 22:43:56 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,18 @@
+public class pattern2 {
+    public static void main(String[] args) {  
+    int i, j, rows=3;            
+    for (i= 0; i<= rows-1; i++) {  
+    for (j=0; j<=i; j++)  {  
+System.out.print("*"+ " ");  
+}   
+System.out.println("");   
+}   
+    for (i=rows-1; i>=0; i--)  {  
+    for(j=0; j <= i-1;j++)  {  
+System.out.print("*"+ " ");  
+}  
+System.out.println("");  
+   }  
+ }  
+    
+}
