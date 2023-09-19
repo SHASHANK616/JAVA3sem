24ffa7d2ce3bad9b3b90b29006641276e3d69ae7
Shashank Suman <sumanshashank616@gmail.com>
2023-09-19 22:43:10 +0530

    Add files via upload

---
+++ intTostr.java
@@ -0,0 +1,8 @@
+// 6.	WAP to convert integer to string.
+public class intTostr {
+    public static void main(String[] args){
+        int n = 5343;
+        String StrNum= Integer.toString(n);
+        System.out.println(StrNum);
+    }
+}
