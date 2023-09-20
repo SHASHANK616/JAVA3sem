89a70e0db10bb5506372aae6fb385d889d4e7535
Shashank Suman <sumanshashank616@gmail.com>
2023-09-20 20:36:39 +0530

    Add files via upload

---
+++ ParseINT.java
@@ -0,0 +1,8 @@
+//5.WAP to parse an integer from a string using parseInt().
+public class ParseINT {
+    public static void main(String [] args){
+        String st = "42";
+        int n = Integer.parseInt(st);
+        System.out.println(n);
+    }
+}
