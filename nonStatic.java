3c049b8f18b53f31903e0fb13e51c4af84378b0a
Shashank Suman <sumanshashank616@gmail.com>
2023-09-22 18:19:17 +0530

    Add files via upload

---
+++ nonStatic.java
@@ -0,0 +1,11 @@
+public class nonStatic {
+    public static void main(String [] args){
+     int a, b,sum;
+     a=10;
+     b=20;
+     sum=a+b;
+     System.out.println(sum);
+     
+    }
+    
+}
