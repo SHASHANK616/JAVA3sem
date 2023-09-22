7561d446faa63a4048009572e5377a7e4ba184b4
Shashank Suman <sumanshashank616@gmail.com>
2023-09-22 18:17:52 +0530

    Add files via upload

---
+++ PDouble.java
@@ -0,0 +1,9 @@
+// 4.WAP to parse a double from a string using parseDouble().
+public class PDouble {
+  public static void main(String [] args){
+    String doubString = "453.35";
+    double parseDouble = Double.parseDouble(doubString);
+    System.out.println("Parse double of string:"+parseDouble);
+  }  
+
+}
