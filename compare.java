d70ec72ca724fbe2be2e8a078a9a1400292021de
Shashank Suman <sumanshashank616@gmail.com>
2023-09-23 13:40:53 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,15 @@
+// 7.	WAP to compare two objects of wrapper class Integer using equals. 
+public class compare {
+     public static void main(String[] args){
+        Integer n1= 50;
+        Integer n2= 50;
+
+ if (n1.equals(n2)){
+    System.out.println("n1 is equal to n2");
+ }
+ else{
+    System.out.println("n1 is not equal to n2");
+ }
+}
+    
+}
